1e23c2e7cd5a3dc8904b6590887269c1b134eb74
Levani Papashvili <papashvililevani@gmail.com>
2020-03-28 18:29:23 -0400

    created country added

--- app.js
+++ app.js
@@ -2,18 +2,24 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
-const countryRoutes = require ('./routes/countryRoutes');
+const countryRoutes = require('./routes/countryRoutes');
+const Country = require('./model/Country')
 
+app.use(bodyParser.json())
+app.use(cors())
+app.use('/' countryRoutes)
 
+// Create a new country
 
-
-const postsRoute = require('./routes/countryRoutes');
-
-
-// Create Routes
-
-app.get('/', (req, res) =>{
-    res.send('We are on home')
+countryRoutes.route('/create').post(function (req, res) {
+  const country = new Country(req.body);
+  country.save()
+    .then(place => {
+      res.json(place);
+    })
+    .catch(err => {
+      res.status(400).send("failed");
+    });
 });
 
 
