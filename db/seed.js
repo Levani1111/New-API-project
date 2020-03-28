148a96fa7fd216e29e1de815586abedc749cdaaa
Levani Papashvili <papashvililevani@gmail.com>
2020-03-28 18:02:26 -0400

    updated seed.js

--- db/seed.js
+++ db/seed.js
@@ -1,3 +1,14 @@
 const mongoose = require('.connection')
 const data = require('./countries.json')
 const Country = require('../model/Country')
+
+Country.deleteMany({}).then(() => {
+    Country
+        .create(data).then((returnData) => {
+            console.log(returnData)
+            process.exit()
+        })
+        .catch((err) => {
+            console.log("our seeding didn't work")
+        })
+})
