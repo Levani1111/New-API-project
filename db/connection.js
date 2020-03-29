d7d796cf19c8dc50d3071dea91a2dbcb74509af8
Levani Papashvili <papashvililevani@gmail.com>
2020-03-29 00:02:51 -0400

    connection.js

--- db/connection.js
+++ db/connection.js
@@ -1,11 +1,8 @@
 const mongoose = require("mongoose");
 
-// using native ES6 Promises, in place of mongoose's deprecated mpromise library
-// `Promise` will provides us with a couple methods: .then() for success,
-// and .catch() for errors
 mongoose.Promise = Promise;
 
-// set the uri for connecting to our local mongodb
+
 let mongoURI = "";
 if (process.env.NODE_ENV === "production") {
   mongoURI = process.env.DB_URL;
@@ -13,15 +10,12 @@ if (process.env.NODE_ENV === "production") {
   mongoURI = "mongodb://127.0.0.1:55294/New-API-project";
 }
 
-// connect to the database, with the imported mongoose instance
+
 mongoose
   .connect(mongoURI, { useNewUrlParser: true })
   .then(instance =>
     console.log(`Connected to db: ${instance.connections[0].name}`)
   )
   .catch(error => console.log("Connection failed!", error));
-
-// now, our mongoose instance has a configured connection to our local db, in addition
-// to its model configuration
 
 module.exports = mongoose;
