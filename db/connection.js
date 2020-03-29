45db6489e1a72a7a8c2e1cb7648741f4a90bc1a3
Levani Papashvili <papashvililevani@gmail.com>
2020-03-29 12:51:48 -0400

    string connection

--- db/connection.js
+++ db/connection.js
@@ -7,7 +7,7 @@ let mongoURI = "";
 
 
 if (process.env.NODE_ENV === "production") {
-        mongoURI = process.env.DB_URL;
+        mongoURI = string(process.env.DB_URL);
 } else {
     mongoURI = "mongodb://localhost/New-API-project";
 }
