4b4277934f7b61fe54a9e95eaea91133edcb4bf5
Levani Papashvili <papashvililevani@gmail.com>
2020-03-29 11:37:24 -0400

    node issue

--- db/connection.js
+++ db/connection.js
@@ -13,11 +13,10 @@ if (process.env.NODE_ENV === "production") {
 }
 
 
-mongoose
-  .connect(mongoURI, { useNewUrlParser: true });
-  .then(instance =>
-    console.log(`Connected to db: ${instance.connections[0].name}`)
-  )
-  .catch(error => console.log("Connection failed!", error));
+mongoose.connect(mongoURI, { useNewUrlParser: true });
+//  .then(instance =>
+//    console.log(`Connected to db: ${instance.connections[0].name}`)
+//  )
+//  .catch(error => console.log("Connection failed!", error));
 
 module.exports = mongoose;
