4256fe61bbd14516ac3041c77985fbaeea9633e6
Levani Papashvili <papashvililevani@gmail.com>
2020-03-29 14:23:17 -0400

    UPDATED DB_URL call

--- db/connection.js
+++ db/connection.js
@@ -7,7 +7,7 @@ let mongoURI = "";
 
 
 if (process.env.NODE_ENV === "production") {
-        mongoURI = process.env.db_URL;
+        mongoURI = process.env.DB_URL;
 } else {
     mongoURI = "mongodb://localhost/country";
 }
